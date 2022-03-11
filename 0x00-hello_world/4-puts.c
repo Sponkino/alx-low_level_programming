a4d07e6760c2e4d5a7745df9b14dbdacfdafe377
Sponkino <meetacmeinc@gmail.com>
2022-03-10 16:33:05 -0800

    my first c running program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
-#!/bin/bash
 #include <stdio.h>
-/**
+/*
  * main- Entry point
- * /
-int main void
+ */
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle"\n);
+	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
-	}
+}

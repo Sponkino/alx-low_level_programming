3ddfbc35adf28d35844f596409ee8351f0d7ddd2
Sponkino <meetacmeinc@gmail.com>
2022-03-10 17:21:14 -0800

    print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /*
  * main- main block
- *
+ *Description: Using the main function this program prints
+ *"programmin is like building a multilingual puzzle
  *  return 0: (Success)
  */
 int main (void)

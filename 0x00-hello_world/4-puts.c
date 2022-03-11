bbaf2237f4d73fec81d86cd1bc216eef604e21ca
Sponkino <meetacmeinc@gmail.com>
2022-03-10 16:38:04 -0800

    Yah first one didnt run perfectly, try this

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,8 @@
 /*
  * main- Entry point
  */
-int main(void)
+int main(void) /*this is the main function*/
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }

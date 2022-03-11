79b273ce7050cafdf113b063654c767d9c4af28a
Sponkino <meetacmeinc@gmail.com>
2022-03-10 16:44:25 -0800

    push

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
  * main- main block
  * return (0)
  */
-int main(void) /*this is the main function*/
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);

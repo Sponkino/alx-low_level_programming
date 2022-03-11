5639e8c107450ae81f31942471ce001744aaf63a
Sponkino <meetacmeinc@gmail.com>
2022-03-11 13:30:11 -0800

    c is fun

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
  * main - main block
- * Description: Use `putchar` function to print any letter alphabet in lowercase.
+ * Description: Use `putchar` function to print any letter alphabet
+ * in lowercase.
  * Return: 0
  */
 int main(void)

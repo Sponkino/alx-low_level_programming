eb6b687d1f7de3eac7fe610c60e914a5783f811e
Sponkino <meetacmeinc@gmail.com>
2022-03-16 02:07:06 -0700

    now

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
-* main - Entry point
+* print_alphabet - Entry point
 * write a program tha prints alpahbets, followed by a new line.
 * Return: Always (Success)
 */
 void print_alphabet(void)
 {	
 	int i;
+
 	for (i = 'a'; i <= 'z'; i++)
 	{
 		_putchar(i);

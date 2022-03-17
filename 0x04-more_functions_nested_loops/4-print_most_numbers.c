642f69f3fa0e78eaaca43792746fb2d2711a2c8e
Sponkino <meetacmeinc@gmail.com>
2022-03-17 14:05:25 -0700

    except 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,12 +7,12 @@
  */
 void print_most_numbers(void);
 {
-	int c;
+	int c = '0';
 
-	for (c = '0'; c <= '9'; c++)
+	for (; c <= '9'; c++)
 	{
 		if (c != '2' && c != '4')
-		_putchar(c);
+			_putchar(c);
 	}
 	_putchar('\n');
 }

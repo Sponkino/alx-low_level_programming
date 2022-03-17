37a01b4b553740ed842b5a09edd59bc48dc628bd
Sponkino <meetacmeinc@gmail.com>
2022-03-17 14:47:49 -0700

    print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,22 +1,21 @@
 #include "main.h"
 
 /**
- * more_numbers - print numbers between 0 to 14
- * 10 times.
- * Return: no return
+ * print_line - check for a digit
+ * @n : number of _ to be printed
+ * Return:void
  */
-void more_numbers(void)
-{
-	int i, ch;
 
-	for (i = 0; i < 10; i++)
+void print_line(int n)
+{
+
+	int i = 0;
+
+	while (i < n && n > 0)
 	{
-		for (ch = 0; ch < 15; ch++)
-		{
-			if (ch >= 10)
-				_putchar((ch / 10) + 48);
-			_putchar((ch % 10) + 48);
-		}
-		_putchar('\n');
+		_putchar('_');
+		i++;
 	}
+	_putchar('\n');
+
 }

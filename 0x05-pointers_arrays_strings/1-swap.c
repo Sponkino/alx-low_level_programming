d0d296fe607557bae7355a6dfe00627ff06c246e
Sponkino <meetacmeinc@gmail.com>
2022-03-21 06:07:51 -0700

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - takes values of a pointer parameter into anther
+ * @a: input integer a.
+ * @b: input integer b
+ * Return: no return.
+ */
+
+void swap_int(int *a, int *b)
+{
+    *a = &b;
+    *b = &a;
+}

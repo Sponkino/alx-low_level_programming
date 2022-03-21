55de738faedfde6b8c37bdbb7588f1ae3a1e16df
Sponkino <meetacmeinc@gmail.com>
2022-03-21 06:31:03 -0700

    swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -8,13 +8,10 @@
  * Return: no return.
  */
 
-void swap_int(void)
+void swap_int(int *a, int *b)
 {
-    int *a;
-    int *b;
-    
-    a = &*a;
-    b = &*b;
-    
-    return (0);
+	*a = &b;
+	*b = &a;
+
+    return (void);
 }

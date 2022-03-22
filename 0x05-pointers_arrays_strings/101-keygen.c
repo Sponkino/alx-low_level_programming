319cd606ffd6c24ae9696520a49c374069456f05
Sponkino <meetacmeinc@gmail.com>
2022-03-21 17:14:08 -0700

    keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,15 +1,22 @@
-#include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
 /**
+ * main - generates random passwords for 101-crackme
+ * Return: zero
  */
 int main(void)
 {
-int num;
+	int sum;
+	char c;
+	srand(time(NULL));
 
-srand(time(0));
-num = rand();
-printf("Anything; %i\n", num);
-return (0);
+	while (sum <= 2645)
+	{
+		c = rand() % 128;
+		sum += c;
+		putchar(c);
+	}
+	putchar(2772 - sum);
+	return (0);
 }

8824da849bf03cc9d4c5de22b30c93fa3383a62d
Sponkino <meetacmeinc@gmail.com>
2022-03-24 13:18:18 -0700

    commit

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,47 +1,37 @@
 #include "main.h"
-#include <stdio.h>
-#include <string.h>
 
 /**
- * *cap_string - this function capitalizes first letter
- * overwriting the null terminate (\0) of dest.
- * @*str: point to string
- * @i: int for word count
- * @output: stores result of comparison.
- * Return: Returns zero
+ * cap_string - captalize strings
+ * @str: variablr srting to capitalize
+ *
+ * Return: value of str
  */
-
 char *cap_string(char *str)
 {
-	int i;
-	
-	for(i = 0; i <= str[i]; i++)
-	{	if (i == 0)
-			{
-				if(str[i] >= 97 && str[i] <= 122)
-				{
-					str[i] -= 32;
-				}
-			}
-		else if(str[i - 1] == ' ' ||
-				str[i - 1] == '\t' ||
-				str[i - 1] == '\n' ||
-				str[i - 1] == ',' ||
-				str[i - 1] == ';' ||
-				str[i - 1] == '.' ||
-				str[i - 1] == '!' ||
-				str[i - 1] == '?' ||
-				str[i - 1] == '"' ||
-				str[i - 1] == '(' ||
-				str[i - 1] == ')' ||
-				str[i- 1] == '{' ||
-				str[i - 1] == '}')
-			{
-				if(str[i] >= 97 && str[i] <= 122)
-				{
-					str[i] -= 32;
-				}
-			}
+	int index = 0;
+
+	while (str[index])
+	{
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
+			index++;
+
+		if (str[index - 1] == ' ' ||
+			str[index - 1] == '\t' ||
+			str[index - 1] == '\n' ||
+			str[index - 1] == ',' ||
+			str[index - 1] == ';' ||
+			str[index - 1] == '.' ||
+			str[index - 1] == '!' ||
+			str[index - 1] == '?' ||
+			str[index - 1] == '"' ||
+			str[index - 1] == '(' ||
+			str[index - 1] == ')' ||
+			str[index - 1] == '{' ||
+			str[index - 1] == '}' ||
+			index == 0)
+			str[index] -= 32;
+		index++;
 	}
-return (0);
+
+	return (str);
 }

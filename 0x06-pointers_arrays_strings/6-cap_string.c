f5f0be24ca83f2109f1395786382287674e9c9ce
Sponkino <meetacmeinc@gmail.com>
2022-03-24 13:08:00 -0700

    capitalize first letter

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * *cap_string - this function capitalizes first letter
+ * overwriting the null terminate (\0) of dest.
+ * @*str: point to string
+ * @i: int for word count
+ * @output: stores result of comparison.
+ * Return: Returns zero
+ */
+
+char *cap_string(char *str)
+{
+	int i;
+	
+	for(i = 0; i <= str[i]; i++)
+	{	if (i == 0)
+			{
+				if(str[i] >= 97 && str[i] <= 122)
+				{
+					str[i] -= 32;
+				}
+			}
+		else if(str[i - 1] == ' ')
+			{
+				if(str[i] >= 97 && str[i] <= 122)
+				{
+					str[i] -= 32;
+				}
+			}
+	}
+return (0);
+}

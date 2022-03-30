ce8b92d7a5e3a6654428c883f4e9866b66b4dbec
Sponkino <meetacmeinc@gmail.com>
2022-03-30 10:26:43 -0700

    first recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - puts recursion function
+ * @s:pointer to function
+ * Return: Returns zero
+ */
+
+void _puts_recursion(char *s)
+{
+puts(s);
+}

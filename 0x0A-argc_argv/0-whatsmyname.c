d5937ebbe9884b0971150207f8e967d3869170b5
Sponkino <sponkino_89@yahoo.co.uk>
2022-04-01 10:42:48 -0700

    argument

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,9 +5,9 @@
  * @argv: array of pointers to arguement strings
  * Return: 0
  **/
-int main(int argc, char **argv[])
+int main(int argc, char *argv[])
 {
 	argc = argc;
-	printf("%s\n", **argv);
+	printf("%s\n", *argv);
 	return (0);
 }

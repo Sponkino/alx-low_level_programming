8d785669422f884bea45056247d01cc8bfdbcf4c
Sponkino <meetacmeinc@gmail.com>
2022-04-11 16:21:05 -0700

    struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,18 @@
 #ifndef DOG_H
 #define DOG_H
 
-#define struct dog
+/**
+ * struct dog - type defining dog as a struct
+ * @name: string of characters
+ * @age: an integer
+ * @owner: string of characters
+ */
+
+typedef struct dog
 {
-	char *name; \
-	float age; \
+	char *name;
+	float age;
 	char *owner;
-}
+} dog;
 
 #endif

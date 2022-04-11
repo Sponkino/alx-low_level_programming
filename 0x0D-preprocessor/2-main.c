153b3e86aa9ddee56fe5f8ebdafd4dd304de1c27
Sponkino <meetacmeinc@gmail.com>
2022-04-11 15:11:15 -0700

    FILE macro

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main function
+ * __FILE__ - macro print file name in string characters
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
+

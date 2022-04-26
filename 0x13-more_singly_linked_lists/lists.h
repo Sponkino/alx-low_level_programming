93db5478bab981443e1c3794709669e15d9891d4
Sponkino <meetacmeinc@gmail.com>
2022-04-26 13:40:34 -0700

    list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -3,21 +3,32 @@
 
 #include <stdlib.h>
 #include <stddef.h>
-
+#include <unistd.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- **/
-
-i
+ *
+ */
 typedef struct listint_s
 {
-	int n;
-	struct listint_s *next;
+    int n;
+    struct listint_s *next;
 } listint_t;
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
